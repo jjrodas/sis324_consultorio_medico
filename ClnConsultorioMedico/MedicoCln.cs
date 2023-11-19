7f589d972d341443b1864840ac7e71d4f01ed9a6
Juan José Rodas Paco <geometrycalero030400@gmail.com>
2023-11-19 02:58:36 -0400

    Creación de PacienteCln y Medicocln y conexión a la base de datos.

---
+++ ClnConsultorioMedico/MedicoCln.cs
@@ -0,0 +1,76 @@
+﻿using CadConsultorioMedico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnConsultorioMedico
+{
+    public class MedicoCln
+    {
+        public static int insertar(Medico medico)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                context.Medico.Add(medico);
+                context.SaveChanges();
+                return medico.id;
+            }
+        }
+
+        public static int actualizar(Medico medico)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                var existente = context.Medico.Find(medico.id);
+                existente.cedulaIdentidad = medico.cedulaIdentidad;
+                existente.nombres = medico.nombres;
+                existente.apellidos = medico.apellidos;
+                existente.direccion = medico.direccion;
+                existente.telefono = medico.telefono;
+                existente.sexo = medico.sexo;
+                existente.matriculaProfesional = medico.matriculaProfesional;
+                existente.fechaNacimiento = medico.fechaNacimiento;
+                existente.usuarioRegistro = medico.usuarioRegistro;
+                existente.estado = medico.estado;
+                return context.SaveChanges();
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                var existente = context.Medico.Find(id);
+                existente.estado = -1;
+                existente.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        public static Medico get(int id)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                return context.Medico.Find(id);
+            }
+        }
+
+        public static List<Medico> listar()
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                return context.Medico.Where(x => x.estado != -1).ToList();
+            }
+        }
+
+        public static List<paMedicoListar_Result> listarPa(string parametro)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                return context.paMedicoListar(parametro).ToList();
+            }
+        }
+    }
+}
---
+++ ClnConsultorioMedico/PacienteCln.cs
@@ -0,0 +1,75 @@
+﻿using CadConsultorioMedico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnConsultorioMedico
+{
+    public class PacienteCln
+    {
+        public static int insertar(Paciente paciente)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                context.Paciente.Add(paciente);
+                context.SaveChanges();
+                return paciente.id;
+            }
+        }
+
+        public static int actualizar(Paciente paciente)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                var existente = context.Paciente.Find(paciente.id);
+                existente.cedulaIdentidad = paciente.cedulaIdentidad;
+                existente.nombres = paciente.nombres;
+                existente.apellidos = paciente.apellidos;
+                existente.direccion = paciente.direccion;
+                existente.telefono = paciente.telefono;
+                existente.sexo = paciente.sexo;
+                existente.fechaNacimiento = paciente.fechaNacimiento;
+                existente.usuarioRegistro = paciente.usuarioRegistro;
+                existente.estado = paciente.estado;
+                return context.SaveChanges();
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                var existente = context.Paciente.Find(id);
+                existente.estado = -1;
+                existente.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        public static Paciente get(int id)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                return context.Paciente.Find(id);
+            }
+        }
+
+        public static List<Paciente> listar()
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                return context.Paciente.Where(x => x.estado != -1).ToList();
+            }
+        }
+
+        public static List<paPacienteListar_Result> listarPa(string parametro)
+        {
+            using (var context = new ConsultorioMedicoEntities())
+            {
+                return context.paPacienteListar(parametro).ToList();
+            }
+        }
+    }
+}
